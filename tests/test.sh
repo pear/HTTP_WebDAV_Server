62f52f08b958a6ef78106edbe4be782cefa267d2
Hartmut Holzgraefe <hholzgra@php.net>
2005-09-24 19:35:35 +0000

    don't set path to litmus binary within the test script
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_WebDAV_Server/trunk@196840 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 (cd ..; pear install -f package.xml)
-pathfront /usr/local/litmus-0.9.3/bin
 rm -f *.log
 mysql -u root webdav -e "TRUNCATE TABLE locks"
 mysql -u root webdav -e "TRUNCATE TABLE properties"
