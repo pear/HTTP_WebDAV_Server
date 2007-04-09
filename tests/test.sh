5c353317dce41100eeb28a9fd133e3c345fac84b
Hartmut Holzgraefe <hholzgra@php.net>
2007-04-09 22:29:43 +0000

    force tests by default
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_WebDAV_Server/trunk@233527 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,7 @@ mysql -u root webdav -e "TRUNCATE TABLE locks"
 mysql -u root webdav -e "TRUNCATE TABLE properties"
 sudo rm -rf /usr/local/apache/htdocs/mod_dav/*
 sudo rm -rf /usr/local/apache/htdocs/litmus/*
-litmus http://localhost/file.php 
+litmus -k http://localhost/file.php   # add -k to continue on errors
 php -q split_log.php
 
 
