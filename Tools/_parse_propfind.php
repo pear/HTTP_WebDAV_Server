2b74fec300425e1b432fe309c6a1cbd20ec9d823
Christian Stocker <chregu@php.net>
2007-07-21 20:02:11 +0000

    change the license to the "New BSD License"
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_WebDAV_Server/trunk@240102 c90b9560-bf6c-de11-be94-00142212c4b1

--- Tools/_parse_propfind.php
+++ Tools/_parse_propfind.php
@@ -5,12 +5,12 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 1997-2003 The PHP Group                                |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
+// | This source file is subject to the New BSD license, That is bundled  |
+// | with this package in the file LICENSE, and is available through      |
+// | the world-wide-web at                                                |
+// | http://www.opensource.org/licenses/bsd-license.php                   |
+// | If you did not receive a copy of the new BSDlicense and are unable   |
+// | to obtain it through the world-wide-web, please send a note to       |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
 // | Authors: Hartmut Holzgraefe <hholzgra@php.net>                       |
